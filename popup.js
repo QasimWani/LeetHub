2cdeee8a10fcaca8cbc73902a2299c5d3bcc7c01
Qasim Wani <qasim31wani@gmail.com>
2020-11-21 12:30:06 +0300

    wip oAuth2.0 working commit

---
+++ popup.js
@@ -0,0 +1,55 @@
+var action = false;
+
+$("#authenticate").on('click', ()=>{
+    if(action)
+    {
+        oAuth2.begin();
+    }
+});
+
+chrome.storage.sync.get("leethub_token", (data)=>{
+    const token = data.leethub_token;
+    if(token == null)
+    {
+        action = true;
+        $("#auth_mode").show();
+    }
+    else
+    {
+        // To validate user, load user object from GitHub.
+        const AUTHENTICATION_URL = "https://api.github.com/user";
+        
+        var xhr = new XMLHttpRequest();
+        xhr.addEventListener('readystatechange', function(event) {
+            console.log(xhr);
+            if(xhr.readyState == 4) {
+                if(xhr.status == 200) {
+                    /* Show MAIN FEATURES */
+                    chrome.storage.sync.get("mode_type", data=>{
+                        if(data && data.mode_type == "commit")
+                        {
+                            $("#commit_mode").show();
+                        }
+                        else
+                        {
+                            $("#hook_mode").show();
+                        }
+                    });
+                }
+                else if(xhr.status == 401) //bad oAuth
+                {
+                    //reset token and redirect to authorization process again!
+                    chrome.storage.sync.set({"leethub_token": null}, data=>{
+                        console.log("BAD oAuth!!! Redirecting back to oAuth process");
+                        action = true;
+                        $("#auth_mode").show();
+                    });
+                }
+            }
+        });
+        xhr.open('GET', AUTHENTICATION_URL, true);
+        xhr.setRequestHeader("Authorization", `token ${token}`);
+        xhr.send();
+    }
+    
+});
