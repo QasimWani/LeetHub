4e4d44d0d28446ba0bca8b2f36319f5b8fb16cd4
Qasim Wani <43754306+QasimWani@users.noreply.github.com>
2020-11-24 04:48:46 +0300

    Create oauth2.js

---
+++ scripts/oauth2.js
@@ -0,0 +1,40 @@
+var oAuth2 = {
+
+    /**
+     * Initialize
+     */
+    init: function() {
+        
+        this.KEY = "leethub_token";
+        this.ACCESS_TOKEN_URL = "https://github.com/login/oauth/access_token";
+        this.AUTHORIZATION_URL = "https://github.com/login/oauth/authorize";
+        this.CLIENT_ID = "YOUR CLIENT ID HERE";
+        this.CLIENT_SECRET = "YOUR CLIENT SECRET HERE";
+        this.REDIRECT_URL = "ENTER REDIRECT URL FOR YOUR APPLICATION"; //for example, https://github.com
+        this.SCOPES = ['repo'];
+    },
+
+    /**
+     * Begin
+     */
+    begin: function() {
+        this.init(); //secure token params.
+
+        var url = this.AUTHORIZATION_URL + "?client_id=" + this.CLIENT_ID + "&redirect_uri" + this.REDIRECT_URL + "&scope=";
+
+        for(var i in this.SCOPES) {
+            url += this.SCOPES[i];
+        }
+        chrome.storage.sync.set({"pipe_leethub" : true}, data=>{ //opening pipe temporarily
+            
+            chrome.tabs.create({url: url, selected: true}, function(data) {
+                window.close();
+                chrome.tabs.getCurrent(function(tab) {
+                    chrome.tabs.remove(tab.id, function(){});
+                });
+            });
+
+        });
+
+    }
+};
